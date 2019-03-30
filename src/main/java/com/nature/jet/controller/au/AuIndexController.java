4c44ac05d7d4eb7b1bd4cd803560c627053e2181
竺志伟 <tdzr_0606@126.com>
2019-03-30 15:10:57 +0800

    细节调整 修改 iframe 页面展示 缓存bug

--- src/main/java/com/nature/jet/controller/au/AuIndexController.java
+++ src/main/java/com/nature/jet/controller/au/AuIndexController.java
@@ -5,6 +5,7 @@ import com.nature.jet.controller.system.BaseController;
 import com.nature.jet.pojo.au.News;
 import com.nature.jet.service.au.NewsService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
