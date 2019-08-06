ef3cf62bca011e4b3d3968604aebed1296cc302d
竺志伟 <tdzr_0606@126.com>
2019-08-06 10:20:11 +0800

    服務器變更為 undertow,添加 ssl配置

--- src/main/java/com/nature/jet/Application.java
+++ src/main/java/com/nature/jet/Application.java
@@ -1,18 +1,15 @@
 package com.nature.jet;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import io.undertow.servlet.api.*;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -29,6 +26,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ServletComponentScan  //打開servlet掃描
 public class Application
 {
+    @Value("${server.port}")
+    Integer httpsPort;
+    @Value("${server.http.port}")
+    Integer httpPort;
+
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class);
@@ -38,17 +40,49 @@ public class Application
     @Bean
     public ServletWebServerFactory servletContainer()
     {
-        JettyServletWebServerFactory jetty = new JettyServletWebServerFactory();
-        jetty.addServerCustomizers(new JettyServerCustomizer()
+        UndertowServletWebServerFactory undertow = new UndertowServletWebServerFactory();
+        //设置IO线程数, 它主要执行非阻塞的任务,它们会负责多个连接, 默认设置每个CPU核心一个线程
+        undertow.setIoThreads(4);
+        //阻塞任务线程池, 当执行类似servlet请求阻塞操作, undertow会从这个线程池中取得线程,它的值设置取决于系统的负载
+        undertow.setWorkerThreads(40);
+        //每块buffer的空间大小,越小的空间被利用越充分
+        undertow.setBufferSize(1024);
+        //是否分配的直接内存
+        undertow.setUseDirectBuffers(true);
+
+        // 添加其他端口监听, 例如 8080 也可以访问,可以添加多个
+        undertow.addBuilderCustomizers(builder ->
         {
-            @Override
-            public void customize(Server server)
-            {
-                final QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
-                threadPool.setMaxThreads(400);
-                threadPool.setMinThreads(20);
-            }
+            builder.addHttpListener(httpPort, "0.0.0.0");
         });
-        return jetty;
+
+        // http 强制 转换 https
+        undertow.addDeploymentInfoCustomizers(deploymentInfo ->
+        {
+            deploymentInfo.addSecurityConstraint(
+                    new SecurityConstraint().addWebResourceCollection(new WebResourceCollection().addUrlPattern("/*"))
+                            .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                            .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+                    .setConfidentialPortManager(exchange -> httpsPort);
+        });
+
+        return undertow;
     }
+
+    //    @Bean
+    //    public ServletWebServerFactory servletContainer()
+    //    {
+    //        JettyServletWebServerFactory jetty = new JettyServletWebServerFactory();
+    //        jetty.addServerCustomizers(new JettyServerCustomizer()
+    //        {
+    //            @Override
+    //            public void customize(Server server)
+    //            {
+    //                final QueuedThreadPool threadPool = server.getBean(QueuedThreadPool.class);
+    //                threadPool.setMaxThreads(400);
+    //                threadPool.setMinThreads(20);
+    //            }
+    //        });
+    //        return jetty;
+    //    }
 }
