67102981cad2ee9bc61f389a8329e0ce5942586f
竺志伟 <tdzr_0606@126.com>
2019-11-18 10:14:51 +0800

    细节修正

--- src/main/java/com/nature/jet/configure/MyWebConfiguer.java
+++ src/main/java/com/nature/jet/configure/MyWebConfiguer.java
@@ -3,10 +3,7 @@ package com.nature.jet.configure;
 import com.nature.jet.interceptor.MyInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 
 /**
@@ -17,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @date :2019-03-19 22:54:38
  */
 @Configuration
-public class MyWebConfiguer extends WebMvcConfigurationSupport
+public class MyWebConfiguer implements WebMvcConfigurer
 {
     @Value("${web.upload-path}")
     String uploadFile;
@@ -30,8 +27,7 @@ public class MyWebConfiguer extends WebMvcConfigurationSupport
      * @author:竺志伟
      * @date :2019-03-19 13:54:07
      */
-    @Override
-    protected void configurePathMatch(PathMatchConfigurer configurer)
+    public void configurePathMatch(PathMatchConfigurer configurer)
     {
         configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(true);
     }
@@ -44,8 +40,7 @@ public class MyWebConfiguer extends WebMvcConfigurationSupport
      * @author:竺志伟
      * @date :2019-03-19 13:55:39
      */
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry)
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
         registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/favicon.ico");
         registry.addResourceHandler("/files/**").addResourceLocations("file:" + uploadFile);
@@ -65,8 +60,7 @@ public class MyWebConfiguer extends WebMvcConfigurationSupport
      * @author:竺志伟
      * @date :2019-03-19 22:49:15
      */
-    @Override
-    protected void addInterceptors(InterceptorRegistry registry)
+    public void addInterceptors(InterceptorRegistry registry)
     {
         registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/", "/error", " /static/**", "/files/**", "/web/login", "/web/loginAction",
